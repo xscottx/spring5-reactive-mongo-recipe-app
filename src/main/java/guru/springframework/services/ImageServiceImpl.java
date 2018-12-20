b538b50b1210e3d4ff6231777c4e9db355a6e9f8
Hoang Vo <hoangvo@gmail.com>
2018-12-20 16:49:02 -0600

    feature: image service reactor'ified

--- src/main/java/guru/springframework/services/ImageServiceImpl.java
+++ src/main/java/guru/springframework/services/ImageServiceImpl.java
@@ -1,11 +1,11 @@
 package guru.springframework.services;
 
 import guru.springframework.domain.Recipe;
-import guru.springframework.repositories.RecipeRepository;
+import guru.springframework.repositories.reactive.RecipeReactiveRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import reactor.core.publisher.Mono;
 
 import java.io.IOException;
 
@@ -17,36 +17,39 @@ import java.io.IOException;
 public class ImageServiceImpl implements ImageService {
 
 
-    private final RecipeRepository recipeRepository;
+    private final RecipeReactiveRepository recipeReactiveRepository;
 
-    public ImageServiceImpl( RecipeRepository recipeService) {
+    public ImageServiceImpl(RecipeReactiveRepository recipeReactiveRepository) {
 
-        this.recipeRepository = recipeService;
+        this.recipeReactiveRepository = recipeReactiveRepository;
     }
 
     @Override
-    @Transactional
-    public void saveImageFile(String recipeId, MultipartFile file) {
+    public Mono<Void> saveImageFile(String recipeId, MultipartFile file) {
 
-        try {
-            Recipe recipe = recipeRepository.findById(recipeId).get();
+        Mono<Recipe> recipeMono = recipeReactiveRepository
+            .findById(recipeId)
+            .map(recipe -> {
+                Byte[] byteObjects;
+                try {
+                    byteObjects = new Byte[file.getBytes().length];
+                    int i = 0;
 
-            Byte[] byteObjects = new Byte[file.getBytes().length];
+                    for (byte b : file.getBytes()) {
+                        byteObjects[i++] = b;
+                    }
 
-            int i = 0;
+                    recipe.setImage(byteObjects);
+                    return recipe;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    throw new RuntimeException(e);
+                }
+            });
 
-            for (byte b : file.getBytes()){
-                byteObjects[i++] = b;
-            }
+        // TODO why do we need to double block here? Block once to wait for image of recipe to save, then another to recipe repo?
+        recipeReactiveRepository.save(recipeMono.block()).block();
 
-            recipe.setImage(byteObjects);
-
-            recipeRepository.save(recipe);
-        } catch (IOException e) {
-            //todo handle better
-            log.error("Error occurred", e);
-
-            e.printStackTrace();
-        }
+        return Mono.empty();
     }
 }
